4728c016861082f379de0537f956656f816a063f
pavinane <pavimega77@gmail.com>
2023-07-24 09:18:26 +0530

    change default structure and more file for flexible work and make resuable

---
+++ app/page.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <section className='w-full flex-center flex-col'>
+      <h1 className='head_text text-center'>
+        Discover & Share     <br className='max-md:hidden'/>
+    
+        <span className=' orange_gradient text-center'>AI-Powered Prompts</span>
+      </h1>
+      <p className='desc text-center'>
+        Prompots is a open source , I try to create clone of this project using Nextjs and tailwind css 
+      </p>
+    </section>
+  )
+}
+
+export default Home
