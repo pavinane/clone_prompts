741513f425cfb4861eae07e5da9977fcc7017961
pavinane <pavimega77@gmail.com>
2023-07-28 23:06:43 +0530

    create feed card

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+'use client'
+import { useState,useEffect } from "react";
+import PromptCard from "./PromptCard";
 
 const Feed = () => {
+  const [searchName,SearchName] = useState();
+  const handleChange = (e) =>{
+
+  }
   return (
-    <div>Feed</div>
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input type="text" 
+        value={searchName}
+        onChange={handleChange}
+        placeholder="Search tag or useName"
+        required
+        className="search_input peer"
+        />
+      </form>
+      <PromptCard/>
+    </section>
   )
 }
 
