61d549e3c0aaf8cd6a443732bbef1cfa0effb92b
pavinane <pavimega77@gmail.com>
2023-07-31 02:20:25 +0530

    Deploy completed

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -20,7 +20,7 @@ const EditPrompt = () => {
         const getPromptDetails = async () => {
             const response = await fetch(`/api/prompt/${promptId}`);
             const data = await response.json();
-            console.log(data,"jbhvhv")
+
 
             setPost({
                 prompt:data.prompt,
